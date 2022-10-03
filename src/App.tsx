03eef42d9ae40c9d329a6ea066f1cc6261a1336c
Ilin Vladislav <ayhalrayz@gmail.com>
2022-10-03 09:50:45 +0300

    feat: className func

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import style from "./App.module.scss";
 import {Routes, Route, Link} from 'react-router-dom';
 import { MainPageLazy } from "./pages/mainPages/MainPage.Lazy";
 import { AboutPageLazy } from "./pages/aboutPage/AboutPage.Lazy";
-import { Suspense, useContext, } from "react";
-import { ThemeContext, Themes } from "./context/theme/ThemeContext";
+import { Suspense } from "react";
 import { useTheme } from "./context/theme/UseTheme";
+import { className } from "./helpers/className";
 
 
 export default function App () {
 	const {theme, changeTheme} = useTheme();
 	return (
-		<div className={`app ${theme}`}>
+		<div className={className('app', {}, [theme])}>
 			<button onClick={changeTheme}>changeTheme</button>
 			<Link to="/about">about</Link>
 			<Link to="/">Main</Link>
---
+++ src/helpers/className.ts
@@ -0,0 +1,11 @@
+type Mode = Record<string, boolean | string>;
+
+export function className(mainClass: string, options: Mode, arrayClass: string[]): string {
+    return [
+        mainClass,
+        ...arrayClass,
+        ...Object.entries(options)
+            .filter(option => !!option[1])
+            .map(option => option[0])
+    ].join(' ')
+}
